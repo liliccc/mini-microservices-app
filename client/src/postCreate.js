8c7d6f1c5b7039b14a19cd4b10f5bd671c14c952
liliccc <xianj.chen@qq.com>
2021-09-28 16:18:06 +0800

    PostList set up

--- client/src/postCreate.js
+++ client/src/postCreate.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const postCreate() => {
+const PostCreate = () => {
+    const [title , setTitle] = useState('');
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+        setTitle('');
+    };
     return (
         <div>
-            
+           <form onSubmit={onSubmit}>
+            <div className="form-group">
+            <label>Title</label>
+            <input value={title} onChange={e => setTitle(e.target.value)} className="from-control" />
+            <button className="btn btn-primary">Submit</button>
+            </div>
+           </form> 
         </div>
     )
 }
 
-export default postCreate
+export default PostCreate
